4e6f221c17543b6b4d4179e864a9734b13c51398
andersoncardoso <anderson.d.cardoso92@gmail.com>
2020-09-27 17:16:41 +0400

    warm up challenge 2

--- dir/run.py
+++ dir/run.py
@@ -8,3 +8,33 @@ def sockMerchant(n, ar):
         else:
             total += int((ar.count(e) - 1) / 2)
     return total
+
+
+#######################
+#######################
+#######################
+steps = 8
+path = ["U", "D", "D", "D", "U", "D", "U", "U"]
+
+
+# Complete the 'countingValleys' function below.
+def countingValleys(s, p):
+    sl = 0
+    bsl = False
+    bsl_c = 0
+    for i, e in enumerate(p):
+        if e == "U":
+            sl += 1
+        elif e == "D":
+            sl -= 1
+
+        if sl < 0 and not bsl:
+            bsl_c += 1
+            bsl = True
+        elif sl == 0 and bsl:
+            bsl = False
+
+    print(bsl_c)
+
+
+countingValleys(steps, path)
